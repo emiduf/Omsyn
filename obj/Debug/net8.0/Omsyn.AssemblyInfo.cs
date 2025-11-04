1202329c4cb87c064a691badcaf103fab96212de
emiduf <emiliedufseth@hotmail.com>
2025-11-04 21:01:11 +0100

    Lagt til Table i View

--- obj/Debug/net8.0/Omsyn.AssemblyInfo.cs
+++ obj/Debug/net8.0/Omsyn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Omsyn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcc937e59ea9234cbc1a637b8f089b6af84d192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143a095873328a75e3e62bf2aa5b24a843d8675d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Omsyn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Omsyn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
